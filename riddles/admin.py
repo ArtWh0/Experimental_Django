bdf1c2e34f360a0abb0a3efae200caa05d1df41a
ArtWh0 <artwh0@yandex.ru>
2019-07-04 12:19:00 +0300

    DJ Commit 002

--- riddles/admin.py
+++ riddles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Option, Riddle
+
+admin.site.register(Riddle)
+
+admin.site.register(Option)
--- riddles/models.py
+++ riddles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Riddle(models.Model):
+	riddle_text=models.CharField(max_length=255)
+	pub_date=models.DateTimeField('date published')
+
+class Option(models.Model):
+	riddle=models.ForeignKey(Riddle, on_delete=models.CASCADE)
+	text=models.CharField(max_length=255)
+	correct=models.BooleanField(default=False)
