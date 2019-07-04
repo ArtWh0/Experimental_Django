990fe9917ff886c89426ff117ba45945e40ebd70
ArtWh0 <artwh0@yandex.ru>
2019-07-04 14:07:50 +0300

    DJ Commit 003

--- riddles/urls.py
+++ riddles/urls.py
@@ -5,4 +5,6 @@ app_name = 'riddles'
 
 urlpatterns=[
 	url(r'^$', views.index, name='index'),
+	url(r'^(?P<riddle_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<riddle_id>[0-9]+)/answer$', views.answer, name='answer')
 ]
--- riddles/views.py
+++ riddles/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Riddle, Option
+
 
 def index(request):
-	return HttpResponse("Hello, my friend!")
+    return render(request, "index.html", {"latest_riddles": Riddle.objects.order_by('-pub_date')[:5]})
 
-# Create your views here.
+
+def detail(request, riddle_id):
+    return render(request, "answer.html", {"riddle": get_object_or_404(Riddle, pk=riddle_id)})
+
+
+def answer(request, riddle_id):
+    riddle = get_object_or_404(Riddle, pk=riddle_id)
+    try:
+        option = riddle.option_set.get(pk=request.POST['option'])
+    except (KeyError, Option.DoesNotExist):
+        return render(request, 'answer.html', {'riddle': riddle, 'error_message': 'Option does not exist'})
+    else:
+        if option.correct:
+            return render(request, "index.html", {"latest_riddles": Riddle.objects.order_by('-pub_date')[:5], "message": "Nice! Choose another one!"})
+        else:
+            return render(request, 'answer.html', {'riddle': riddle, 'error_message': 'Wrong Answer!'})
